573d3adc90f97b3f485179777f464d6fd786cbff
William Harvey <williamnharvey@gmail.com>
2019-11-12 17:41:45 -0500

    removed commented code and set dashboard as default (#4)

--- app/src/main/java/com/pillpals/pillbuddies/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/pillpals/pillbuddies/ui/dashboard/DashboardFragment.kt
@@ -39,7 +39,7 @@ class DashboardFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
-        val view = inflater!!.inflate(R.layout.fragment_dashboard,container,false)
+        val view = inflater!!.inflate(R.layout.fragment_dashboard, container,false)
 
         realm = Realm.getDefaultInstance()
 
