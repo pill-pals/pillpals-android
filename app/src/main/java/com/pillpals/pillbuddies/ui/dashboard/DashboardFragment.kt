88711b66866154e33b168622af7b8a8e4f3c3e33
Christiaan Oostenbrug <christiaan.oostenbrug@gmail.com>
2019-11-12 16:22:45 -0500

    adds frame of tab bar

---
+++ app/src/main/java/com/pillpals/pillbuddies/ui/dashboard/DashboardFragment.kt
@@ -0,0 +1,33 @@
+package com.pillpals.pillbuddies.ui.dashboard
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.pillpals.pillbuddies.R
+
+class DashboardFragment : Fragment() {
+
+    private lateinit var dashboardViewModel: DashboardViewModel
+    /*
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        dashboardViewModel =
+            ViewModelProvider(DashboardViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
+        val textView: TextView = root.findViewById(R.id.text_dashboard)
+        dashboardViewModel.text.observe(this, Observer {
+            textView.text = it
+        })
+        return root
+    }
+
+     */
+}
