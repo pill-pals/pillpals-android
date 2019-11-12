fcbb343b09f8838efe8ca8c8d913461ddccf5882
William Harvey <williamnharvey@gmail.com>
2019-11-12 16:21:41 -0500

    Create db relationships (#1)
    * set up db relationships and manual tests
    
    * moved test data creation out of onCreate

--- app/src/main/java/com/pillpals/pillbuddies/data/model/Schedules.kt
+++ app/src/main/java/com/pillpals/pillbuddies/data/model/Schedules.kt
@@ -2,10 +2,17 @@ package com.pillpals.pillbuddies.data.model
 
 import io.realm.RealmObject
 import java.util.Date
+import io.realm.RealmList
+import io.realm.annotations.PrimaryKey
+import io.realm.annotations.LinkingObjects
+import io.realm.RealmResults
 
 open class Schedules(
+    @PrimaryKey var uid: String? = null,
     var occurrence: Date? = null,
     var repetitionCount: Int? = null,
     var repetitionUnit: Int? = null,
-    var uid: String? = null
+    var logs: RealmList<Logs> = RealmList(),
+    @LinkingObjects("schedules")
+    val medication: RealmResults<Medications>? = null
 ) : RealmObject(){}
--- app/src/main/java/com/pillpals/pillbuddies/ui/MainActivity.kt
+++ app/src/main/java/com/pillpals/pillbuddies/ui/MainActivity.kt
@@ -5,6 +5,17 @@ import android.os.Bundle
 import android.widget.LinearLayout
 import io.realm.Realm
 import com.pillpals.pillbuddies.R
+import io.realm.RealmObject
+import com.pillpals.pillbuddies.data.model.Medications
+import com.pillpals.pillbuddies.data.model.Schedules
+import io.realm.RealmResults
+import java.util.UUID
+import java.util.Date
+import java.util.Calendar
+import com.pillpals.pillbuddies.helpers.DateHelper
+
+import android.util.Log
+
 
 class MainActivity : AppCompatActivity() {
     public lateinit var currentStack: LinearLayout
@@ -25,9 +36,18 @@ class MainActivity : AppCompatActivity() {
         upcomingStack = findViewById(R.id.upcomingStack)
         completedStack = findViewById(R.id.completedStack)
 
+        // Testing
+        //clearDatabase()
+        createTestData()
         populateAllStacks(8)
     }
 
+    private fun readAllData(realmClass: Class<out RealmObject>): RealmResults<out RealmObject> {
+        return realm.where(realmClass).findAll()
+    }
+
+    // region
+    // Testing functions
     private fun populateAllStacks(n: Int) {
         val testCards = Array(n) { DrugCard(this) }
 
@@ -40,4 +60,70 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
+    private fun createTestData() {
+        var medications = readAllData(Medications::class.java) as RealmResults<Medications>
+        if (medications.count() == 0) {
+            createTestMedicationData()
+            medications = readAllData(Medications::class.java) as RealmResults<Medications>
+        }
+        var schedules = readAllData(Schedules::class.java) as RealmResults<Schedules>
+        if (schedules.count() == 0) {
+            createTestSchedulesData(medications.first()!!)
+            schedules = readAllData(Schedules::class.java) as RealmResults<Schedules>
+        }
+        Log.i("yeet", schedules.first()?.occurrence.toString())
+        Log.i("yeet", medications.first()?.name)
+    }
+
+    private fun clearDatabase() {
+        realm.beginTransaction()
+        realm.deleteAll()
+        realm.commitTransaction()
+    }
+
+    private fun createTestMedicationData() {
+        realm.executeTransaction {
+            val medication = it.createObject(Medications::class.java, UUID.randomUUID().toString())
+            medication.name = "Database Drug 1"
+            medication.dosage = "50mg"
+        }
+    }
+
+    private fun createTestSchedulesData(medication: Medications) {
+        realm.executeTransaction {
+            val firstSchedule = it.createObject(Schedules::class.java, UUID.randomUUID().toString())
+
+            val date = Date()
+            val cal = Calendar.getInstance()
+            cal.time = date
+            cal.set(Calendar.MILLISECOND, 0)
+            cal.set(Calendar.SECOND, 0)
+            cal.set(Calendar.MINUTE, 0)
+            cal.set(Calendar.HOUR_OF_DAY, 8)
+
+            firstSchedule.occurrence = cal.time
+
+            firstSchedule.repetitionCount = 1
+            firstSchedule.repetitionUnit = DateHelper.getIndexByUnit(Calendar.DAY_OF_MONTH)
+            medication.schedules.add(firstSchedule)
+
+            val secondSchedule = it.createObject(Schedules::class.java, UUID.randomUUID().toString())
+
+            val cal2 = Calendar.getInstance()
+            cal2.time = date
+            cal2.set(Calendar.MILLISECOND, 0)
+            cal2.set(Calendar.SECOND, 0)
+            cal2.set(Calendar.MINUTE, 0)
+            cal2.set(Calendar.HOUR_OF_DAY, 8)
+            secondSchedule.occurrence = cal2.time
+
+            secondSchedule.repetitionCount = 1
+            secondSchedule.repetitionUnit = DateHelper.getIndexByUnit(Calendar.DAY_OF_MONTH)
+            medication.schedules.add(secondSchedule)
+        }
+    }
+
+    // endregion
+
 }
