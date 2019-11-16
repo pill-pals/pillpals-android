fa53aba7da1101ab6c9d264f630730821d2c723c
William Harvey <williamnharvey@gmail.com>
2019-11-16 18:12:27 -0500

    fix notifications (#15)

--- app/src/main/java/com/pillpals/pillbuddies/helpers/NotificationUtil.kt
+++ app/src/main/java/com/pillpals/pillbuddies/helpers/NotificationUtil.kt
@@ -1,4 +1,4 @@
-package com.pillpals.pillbuddies.services
+package com.pillpals.pillbuddies.helpers
 
 import android.app.AlarmManager
 import android.app.PendingIntent
@@ -6,8 +6,8 @@ import android.content.Context
 import android.content.Intent
 import com.pillpals.pillbuddies.data.model.Schedules
 import java.util.*
-import com.pillpals.pillbuddies.helpers.DateHelper
 import android.util.Log
+import com.pillpals.pillbuddies.services.AlarmReceiver
 
 class NotificationUtils {
     companion object {
@@ -15,7 +15,7 @@ class NotificationUtils {
             val intent = Intent(context, AlarmReceiver::class.java)
             intent.putExtra("schedule-uid", schedule.uid)
             intent.putExtra("schedule-occurrence", DateHelper.convertToLocalDateViaInstant(schedule.occurrence!!).toString())
-            Log.i("oo",  DateHelper.convertToLocalDateViaInstant(schedule.occurrence!!).toString())
+
             val mAlarmSender = PendingIntent.getBroadcast(
                 context,
                 schedule.uid!!.hashCode(),
@@ -24,10 +24,8 @@ class NotificationUtils {
             )
 
             val c = Calendar.getInstance()
-            c.add(Calendar.SECOND, 2)
-            //c.time = schedule.occurrence
+            c.time = schedule.occurrence
             val firstTime = c.timeInMillis
-            Log.i("test", firstTime.toString())
 
             val am = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
             val interval = DateHelper.getMillisecondsByUnit(
