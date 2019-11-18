968dae4df04e1a29c0db4a5941c675e6020a3d2c
Christiaan Oostenbrug <christiaan.oostenbrug@gmail.com>
2019-11-18 16:02:26 -0500

    creates some drug schedule records on adddrug page

--- app/src/main/java/com/pillpals/pillbuddies/ui/AddDrugActivity.kt
+++ app/src/main/java/com/pillpals/pillbuddies/ui/AddDrugActivity.kt
@@ -10,12 +10,7 @@ import android.widget.LinearLayout
 import android.widget.Toast
 import java.util.Calendar
 
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import com.pillpals.pillbuddies.R
 import com.pillpals.pillbuddies.data.model.Medications
 import com.pillpals.pillbuddies.helpers.DatabaseHelper.Companion.getMedicationByUid
@@ -27,8 +22,8 @@ import kotlinx.android.synthetic.main.bottom_options.*
 import kotlinx.android.synthetic.main.delete_prompt.view.*
 import kotlinx.android.synthetic.main.prompts.view.*
 import kotlinx.android.synthetic.main.prompts.view.dialogCancelBtn
+
 import java.util.*
-import kotlin.collections.ArrayList
 import com.pillpals.pillbuddies.data.model.Schedules
 import com.pillpals.pillbuddies.helpers.DateHelper
 
@@ -56,10 +51,6 @@ class AddDrugActivity : AppCompatActivity() {
         bottomOptions.leftButton.text = "Save"
         bottomOptions.rightButton.text = "Cancel"
 
-
-        val testSchedule = ScheduleRecord(this)
-        scheduleStack.addView(testSchedule)
-
         if (intent.hasExtra("medication-uid")) {
             val medID: String = intent.getStringExtra("medication-uid")
             val medication = getMedicationByUid(medID) as Medications
@@ -69,17 +60,22 @@ class AddDrugActivity : AppCompatActivity() {
             editText3.setText(medication.notes)
 
             var scheduleRecords = MutableList(0) { ScheduleRecord(this) }
+
             medication.schedules.forEach {
                 if(isWeeklyRecurrence(it)) {
 
+                } else {
+                    val scheduleRecord = ScheduleRecord(this)
+                        scheduleRecord.timeText.text = DateHelper.dateToString(it.occurrence!!)
+                        scheduleRecord.recurrenceText.text = "every"
+                        scheduleRecord.dateText.text = getRecurrenceString(it.repetitionUnit!!,it.repetitionCount!!)
+                        scheduleRecords.add(scheduleRecord)
                 }
             }
-            //group the medications by time if recurrence type is weekly
-            //create a ScheduleRecord for each group
-            //timeText => time with AM/PM
-            //recurrenceText => determined by recurrence type
-            //dateText => determined by recurrence type and entries in the group
-            //add all scheduleRecords to the stack
+
+            scheduleRecords.forEach {
+                scheduleStack.addView(it)
+            }
 
             bottomOptions.leftButton.setOnClickListener{
                 if (editText.text.toString().trim().isNotEmpty() and editText2.text.toString().trim().isNotEmpty()) {
@@ -172,6 +168,30 @@ class AddDrugActivity : AppCompatActivity() {
         //val a = (schedule.repetitionCount == 7 && DateHelper.getUnitByIndex(schedule.repetitionUnit!!) == Calendar.DATE)
         //val b = (schedule.repetitionCount == 1 && DateHelper.getUnitByIndex(schedule.repetitionUnit!!) == Calendar.WEEK)
         return (schedule.repetitionCount == 7 && DateHelper.getUnitByIndex(schedule.repetitionUnit!!) == Calendar.DATE)//a || b
+    }
+
+    private fun getRecurrenceString(repetitionUnit: Int, repetitionCount: Int):String {
+        if (repetitionCount == 1) {
+            return when (DateHelper.getUnitByIndex(repetitionUnit)) {
+                Calendar.YEAR -> "year"
+                Calendar.MONTH -> "month"
+                Calendar.DATE -> "day"
+                Calendar.HOUR_OF_DAY -> "hour"
+                Calendar.MINUTE -> "minute"
+                Calendar.SECOND -> "second"
+                else -> "MISSING"
+            }
+        } else {
+            return when (DateHelper.getUnitByIndex(repetitionUnit)) {
+                Calendar.YEAR -> "$repetitionCount years"
+                Calendar.MONTH -> "$repetitionCount months"
+                Calendar.DATE -> "$repetitionCount days"
+                Calendar.HOUR_OF_DAY -> "$repetitionCount hours"
+                Calendar.MINUTE -> "$repetitionCount minutes"
+                Calendar.SECOND ->  "$repetitionCount seconds"
+                else -> "MISSING"
+            }
+        }
     }
 }
 
