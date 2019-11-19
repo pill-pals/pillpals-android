9f772f3cd738397077defa40735f239ea50315db
Christiaan Oostenbrug <46493392+coostenbrug@users.noreply.github.com>
2019-11-18 22:28:26 -0500

    Adds Schedule List UI to Medication Edit page (#26)
    * adds ScheduleRecord view object
    
    * creates some drug schedule records on adddrug page
    
    * supports weekly recurrence in schedule display
    
    * UI tweaks
    
    * adds sample schedule data
    
    * creates compiledScheduleRecords from scheduleRecords that have the same time and are tracked by weekday and displays it in the UI

--- app/src/main/java/com/pillpals/pillbuddies/ui/AddDrugActivity.kt
+++ app/src/main/java/com/pillpals/pillbuddies/ui/AddDrugActivity.kt
@@ -4,30 +4,37 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.widget.*
 import androidx.appcompat.app.AlertDialog
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.Toast
+import java.util.Calendar
+
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import com.pillpals.pillbuddies.R
 import com.pillpals.pillbuddies.data.model.Medications
 import com.pillpals.pillbuddies.helpers.DatabaseHelper.Companion.getMedicationByUid
 import io.realm.Realm
+import io.realm.RealmObject
+import io.realm.RealmResults
 import kotlinx.android.synthetic.main.activity_add_drug.*
 import kotlinx.android.synthetic.main.bottom_options.*
 import kotlinx.android.synthetic.main.delete_prompt.view.*
 import kotlinx.android.synthetic.main.prompts.view.*
 import kotlinx.android.synthetic.main.prompts.view.dialogCancelBtn
-import java.util.*
 
-import android.util.Log
+import java.util.*
+import com.pillpals.pillbuddies.data.model.Schedules
+import com.pillpals.pillbuddies.helpers.DateHelper
+import android.util.Log;
+
 class AddDrugActivity : AppCompatActivity() {
 
     public lateinit var editText: EditText
     public lateinit var editText2: EditText
     public lateinit var editText3: EditText
     public lateinit var deleteButton: TextView
+    public lateinit var scheduleStack: LinearLayout
     public lateinit var bottomOptions: BottomOptions
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,6 +46,7 @@ class AddDrugActivity : AppCompatActivity() {
         editText2 = findViewById(R.id.editText2)
         editText3 = findViewById(R.id.editText3)
         deleteButton = findViewById(R.id.deleteButton)
+        scheduleStack = findViewById(R.id.scheduleStack)
         bottomOptions = findViewById(R.id.bottomOptions)
         bottomOptions.leftButton.text = "Save"
         bottomOptions.rightButton.text = "Cancel"
@@ -50,6 +58,48 @@ class AddDrugActivity : AppCompatActivity() {
             editText.setText(medication.name)
             editText2.setText(medication.dosage)
             editText3.setText(medication.notes)
+
+            var scheduleRecords = MutableList(0) { ScheduleRecord(this) }
+            var compiledScheduleRecords = MutableList(0) { CompiledScheduleRecord("")}
+
+            medication.schedules.forEach {
+                val timeString = DateHelper.dateToString(it.occurrence!!)
+                if(isWeeklyRecurrence(it)) {
+                    val cal = Calendar.getInstance()
+                    cal.time = it.occurrence
+                    if(compiledScheduleRecordExists(timeString, compiledScheduleRecords)) {
+                        //add day of week to existing compiled schedule record
+                        val compiledScheduleRecord = compiledScheduleRecords.find {it.time == timeString}
+                        compiledScheduleRecord!!.daysOfWeek[cal.get(Calendar.DAY_OF_WEEK) - 1] = 1
+                    } else {
+                        //create new compiled schedule record
+                        val compiledScheduleRecord = CompiledScheduleRecord(timeString)
+                        compiledScheduleRecord.daysOfWeek[cal.get(Calendar.DAY_OF_WEEK) - 1] = 1
+                        compiledScheduleRecords.add(compiledScheduleRecord)
+                    }
+                } else {
+                    val scheduleRecord = ScheduleRecord(this)
+
+                    scheduleRecord.timeText.text = timeString
+                    scheduleRecord.recurrenceText.text = "every"
+                    scheduleRecord.dateText.text = getRecurrenceString(it.repetitionUnit!!,it.repetitionCount!!)
+                    scheduleRecords.add(scheduleRecord)
+                }
+            }
+
+            //create scheduleRecords from compiledScheduleRecords
+            compiledScheduleRecords.forEach {
+                val scheduleRecord = ScheduleRecord(this)
+
+                scheduleRecord.timeText.text = it.time
+                scheduleRecord.recurrenceText.text = "on"
+                scheduleRecord.dateText.text = getDaysOfWeekList(it.daysOfWeek)
+                scheduleRecords.add(scheduleRecord)
+            }
+
+            scheduleRecords.forEach {
+                scheduleStack.addView(it)
+            }
 
             bottomOptions.leftButton.setOnClickListener{
                 if (editText.text.toString().trim().isNotEmpty() and editText2.text.toString().trim().isNotEmpty()) {
@@ -64,7 +114,7 @@ class AddDrugActivity : AppCompatActivity() {
                     Toast.makeText(applicationContext, "Please set a name and dosage", Toast.LENGTH_SHORT).show()
                 }
             }
-        } else{
+        } else {
             bottomOptions.leftButton.setOnClickListener{
                 if (editText.text.toString().trim().isNotEmpty() and editText2.text.toString().trim().isNotEmpty()) {
                     createMedicationData(
@@ -137,5 +187,65 @@ class AddDrugActivity : AppCompatActivity() {
             medication.deleted = true
         }
     }
+
+    private fun isWeeklyRecurrence(schedule: Schedules):Boolean {
+        val a = (schedule.repetitionCount == 7 && DateHelper.getUnitByIndex(schedule.repetitionUnit!!) == Calendar.DATE)
+        val b = (schedule.repetitionCount == 1 && DateHelper.getUnitByIndex(schedule.repetitionUnit!!) == Calendar.WEEK_OF_YEAR)
+        return (a || b)
+    }
+
+    private fun getRecurrenceString(repetitionUnit: Int, repetitionCount: Int):String {
+        if (repetitionCount == 1) {
+            return when (DateHelper.getUnitByIndex(repetitionUnit)) {
+                Calendar.YEAR -> "year"
+                Calendar.MONTH -> "month"
+                Calendar.WEEK_OF_YEAR -> "week"
+                Calendar.DATE -> "day"
+                Calendar.HOUR_OF_DAY -> "hour"
+                Calendar.MINUTE -> "minute"
+                Calendar.SECOND -> "second"
+                else -> "MISSING"
+            }
+        } else {
+            return when (DateHelper.getUnitByIndex(repetitionUnit)) {
+                Calendar.YEAR -> "$repetitionCount years"
+                Calendar.MONTH -> "$repetitionCount months"
+                Calendar.WEEK_OF_YEAR -> "$repetitionCount weeks"
+                Calendar.DATE -> "$repetitionCount days"
+                Calendar.HOUR_OF_DAY -> "$repetitionCount hours"
+                Calendar.MINUTE -> "$repetitionCount minutes"
+                Calendar.SECOND ->  "$repetitionCount seconds"
+                else -> "MISSING"
+            }
+        }
+    }
+
+    private fun compiledScheduleRecordExists(timeString: String, compiledScheduleRecords: MutableList<CompiledScheduleRecord>):Boolean {
+        for (i in compiledScheduleRecords.indices) {
+            if(timeString == compiledScheduleRecords[i].time)
+                return true
+        }
+        return false
+    }
+
+    private fun getDaysOfWeekList(daysOfWeek: IntArray):String {
+        val daysOfWeekList = listOf("Sun","Mon","Tue","Wed","Thurs","Fri","Sat")
+        var list = ""
+        var firstVal = true
+        for (i in daysOfWeek.indices) {
+            if (daysOfWeek[i] == 1 && firstVal) {
+                list += daysOfWeekList[i]
+                firstVal = false
+            } else if (daysOfWeek[i] == 1) {
+                list += ", ${daysOfWeekList[i]}"
+            }
+        }
+        return list
+    }
 }
+
+data class CompiledScheduleRecord(val time: String) {
+    var daysOfWeek: IntArray = IntArray(7) {0}
+}
+
 
--- app/src/main/java/com/pillpals/pillbuddies/ui/ScheduleRecord.kt
+++ app/src/main/java/com/pillpals/pillbuddies/ui/ScheduleRecord.kt
@@ -4,27 +4,19 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.util.AttributeSet
-import android.widget.ImageView
 import android.widget.TextView
 import android.widget.LinearLayout
-import com.google.android.material.button.MaterialButton
 import com.pillpals.pillbuddies.R
-import androidx.cardview.widget.CardView
 
-class DrugCard : LinearLayout {
+class ScheduleRecord : LinearLayout {
 
     private val VIEW_CHANGED = true
     private val VIEW_NOT_CHANGED = false
     private val DEFAULT_COLOR = Color.DKGRAY
 
-    public lateinit var drugCard: CardView
-    public lateinit var nameText: TextView
-    public lateinit var altText: TextView
-    public lateinit var lateText: TextView
-    public lateinit var button: MaterialButton
-    public lateinit var countdownLabel: TextView
-    public lateinit var icon: ImageView
-    public lateinit var doneImage: ImageView
+    public lateinit var timeText: TextView
+    public lateinit var recurrenceText: TextView
+    public lateinit var dateText: TextView
 
     companion object {
         private var mSquareColor: Int = 0
@@ -49,23 +41,13 @@ class DrugCard : LinearLayout {
         //Inflate xml resource, pass "this" as the parent, we use <merge> tag in xml to avoid
         //redundant parent, otherwise a LinearLayout will be added to this LinearLayout ending up
         //with two view groups
-        inflate(this.context, R.layout.drug_card,this)
+        inflate(this.context, R.layout.schedule_record,this)
 
         //Get references to elements
-        drugCard = findViewById(R.id.LogCard)
-        altText  = findViewById(R.id.timeText)
-        lateText  = findViewById(R.id.lateText)
-        nameText  = findViewById(R.id.nameText)
-        button  = findViewById(R.id.button)
-        countdownLabel = findViewById(R.id.countdownLabel)
-        icon = findViewById(R.id.icon)
-        doneImage = findViewById(R.id.doneImage)
+        timeText = findViewById(R.id.timeText)
+        recurrenceText = findViewById(R.id.recurrenceText)
+        dateText = findViewById(R.id.dateText)
 
-        //Initialize elements
-        button.visibility = GONE
-        doneImage.visibility = GONE
-        countdownLabel.visibility = GONE
-        lateText.visibility = GONE
     }
 
     override fun onDraw(canvas: Canvas) {
