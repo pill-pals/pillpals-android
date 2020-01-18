894dd5c7fe1b66448c3f51c78d227d91c5261e4e
Lucas Van de Mosselaer <lucvdm5@gmail.com>
2020-01-18 17:22:00 -0500

    Connected 'Add' button to AddDrugActivity and switched to FragmentStatePagerAdapter to fix blank ViewPager bug

--- app/src/main/java/com/pillpals/pillbuddies/ui/medications/medication_info/MedicationInfoFragment.kt
+++ app/src/main/java/com/pillpals/pillbuddies/ui/medications/medication_info/MedicationInfoFragment.kt
@@ -1,24 +1,26 @@
 package com.pillpals.pillbuddies.ui.medications.medication_info
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.FrameLayout
+import android.widget.Button
 import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentPagerAdapter
-import androidx.fragment.app.FragmentTransaction
+import androidx.fragment.app.FragmentStatePagerAdapter
 import androidx.viewpager.widget.ViewPager
 import com.google.android.material.tabs.TabLayout
 
 import com.pillpals.pillbuddies.R
+import com.pillpals.pillbuddies.ui.AddDrugActivity
 import io.realm.Realm
 
 class MedicationInfoFragment : Fragment() {
 
     public lateinit var tabLayout: TabLayout
     public lateinit var tabViewPager: ViewPager
+    public lateinit var addButton: Button
 
     private lateinit var realm: Realm
 
@@ -35,13 +37,14 @@ class MedicationInfoFragment : Fragment() {
         "Reviews"
     )
     private lateinit var tabPagerAdapter: TabPagerAdapter
-    
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         super.onCreateView(inflater, container, savedInstanceState)
         val view = inflater!!.inflate(R.layout.fragment_medication_info, container,false)
 
         realm = Realm.getDefaultInstance()
 
+        addButton = view!!.findViewById(R.id.addButton)
         tabLayout = view!!.findViewById(R.id.tabLayout)
         tabViewPager = view!!.findViewById(R.id.tabViewPager)
         tabLayout.setupWithViewPager(tabViewPager)
@@ -49,10 +52,15 @@ class MedicationInfoFragment : Fragment() {
         tabPagerAdapter = TabPagerAdapter(activity!!.supportFragmentManager)
         tabViewPager.adapter = tabPagerAdapter
 
+        addButton.setOnClickListener {
+            val intent = Intent(context, AddDrugActivity::class.java)
+            startActivityForResult(intent, 1)
+        }
+
         return view
     }
 
-    private inner class TabPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+    private inner class TabPagerAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
         override fun getItem(position: Int) : Fragment {
             return tabFragments[position]
         }
