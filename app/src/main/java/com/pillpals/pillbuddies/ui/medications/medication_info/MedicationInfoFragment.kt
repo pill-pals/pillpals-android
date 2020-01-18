65b53e6c79f79a02a234407f0b4d90663bf1d0eb
Lucas Van de Mosselaer <lucvdm5@gmail.com>
2020-01-18 17:11:17 -0500

    Replaced FrameLayout and manual fragment loading with ViewPager for tab switching

--- app/src/main/java/com/pillpals/pillbuddies/ui/medications/medication_info/MedicationInfoFragment.kt
+++ app/src/main/java/com/pillpals/pillbuddies/ui/medications/medication_info/MedicationInfoFragment.kt
@@ -7,7 +7,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
 import androidx.fragment.app.FragmentTransaction
+import androidx.viewpager.widget.ViewPager
 import com.google.android.material.tabs.TabLayout
 
 import com.pillpals.pillbuddies.R
@@ -16,13 +18,24 @@ import io.realm.Realm
 class MedicationInfoFragment : Fragment() {
 
     public lateinit var tabLayout: TabLayout
-    public lateinit var contentFrame: FrameLayout
+    public lateinit var tabViewPager: ViewPager
 
     private lateinit var realm: Realm
 
-    private lateinit var tabFragments: List<Fragment>
-    private var loadingTab: Boolean = false
-
+    private var tabFragments: List<Fragment> = mutableListOf(
+        MedicationInfoTextFragment(),
+        MedicationInfoTextFragment(),
+        MedicationInfoTextFragment(),
+        MedicationInfoTextFragment()
+    )
+    private var tabTitles: List<String> = mutableListOf(
+        "Overview",
+        "Side Effects",
+        "Tips",
+        "Reviews"
+    )
+    private lateinit var tabPagerAdapter: TabPagerAdapter
+    
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         super.onCreateView(inflater, container, savedInstanceState)
         val view = inflater!!.inflate(R.layout.fragment_medication_info, container,false)
@@ -30,39 +43,26 @@ class MedicationInfoFragment : Fragment() {
         realm = Realm.getDefaultInstance()
 
         tabLayout = view!!.findViewById(R.id.tabLayout)
-        contentFrame = view!!.findViewById(R.id.tabContentFrame)
+        tabViewPager = view!!.findViewById(R.id.tabViewPager)
+        tabLayout.setupWithViewPager(tabViewPager)
 
-        //TODO: Add these in a loop or something (they probably won't all be text fragments eventually anyway)
-        tabFragments = mutableListOf(MedicationInfoTextFragment(), MedicationInfoTextFragment(), MedicationInfoTextFragment(), MedicationInfoTextFragment())
-
-        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
-            override fun onTabSelected(tab: TabLayout.Tab) {
-                loadTabContent(tab.position)
-            }
-
-            override fun onTabReselected(tab: TabLayout.Tab) {
-                //TODO: Action if the currently selected tab is clicked again (could scroll back to top)
-            }
-
-            override fun onTabUnselected(tab: TabLayout.Tab) {
-                //TODO: Action if a different tab is selected
-            }
-        })
-
-        loadTabContent(0) //Initialize default (first) tab
+        tabPagerAdapter = TabPagerAdapter(activity!!.supportFragmentManager)
+        tabViewPager.adapter = tabPagerAdapter
 
         return view
     }
 
-    private fun loadTabContent(tabInd: Int) {
-        if (tabFragments.size != 0 && tabFragments.size > tabInd && !loadingTab) {
-            loadingTab = true
-            var fm : FragmentManager = activity!!.supportFragmentManager
-            var ft : FragmentTransaction = fm.beginTransaction()
-            ft.replace(R.id.tabContentFrame, tabFragments[tabInd])
-            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-            ft.commit()
-            loadingTab = false
+    private inner class TabPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+        override fun getItem(position: Int) : Fragment {
+            return tabFragments[position]
+        }
+
+        override fun getCount() : Int {
+            return tabFragments.size
+        }
+
+        override fun getPageTitle(position: Int) : String {
+            return tabTitles[position]
         }
     }
 }
