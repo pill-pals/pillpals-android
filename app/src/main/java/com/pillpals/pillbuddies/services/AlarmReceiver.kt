156184095fc186037e07ddd76bd214a873e19151
William Harvey <williamnharvey@gmail.com>
2020-01-23 18:11:10 -0500

    Cancel notifications on click or log, and icon (#86)

--- app/src/main/java/com/pillpals/pillbuddies/services/AlarmReceiver.kt
+++ app/src/main/java/com/pillpals/pillbuddies/services/AlarmReceiver.kt
@@ -4,6 +4,7 @@ import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.graphics.*
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.pillpals.pillbuddies.R
@@ -16,6 +17,14 @@ import android.util.Log
 import com.pillpals.pillbuddies.ui.MainActivity
 import java.time.LocalDateTime
 import java.time.ZoneId
+import android.opengl.ETC1.getHeight
+import android.opengl.ETC1.getWidth
+import android.graphics.drawable.BitmapDrawable
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
+import com.pillpals.pillbuddies.helpers.DatabaseHelper.Companion.getColorStringByID
+import com.pillpals.pillbuddies.helpers.DatabaseHelper.Companion.getDrawableIconById
+
 
 public class AlarmReceiver: BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
@@ -35,15 +44,33 @@ public class AlarmReceiver: BroadcastReceiver() {
 
         val pendingIntent = PendingIntent.getActivity(context, schedule.uid!!.hashCode(), mainIntent, PendingIntent.FLAG_UPDATE_CURRENT)
 
+        val drawable = ContextCompat.getDrawable(context, getDrawableIconById(context, medication.icon_id))!!
+
+        val iconBitmap = Bitmap.createBitmap((drawable.intrinsicWidth * 1.1).toInt(), (drawable.intrinsicHeight * 1.1).toInt(), Bitmap.Config.ARGB_8888)
+
+        val iconCanvas = Canvas(iconBitmap)
+        val paint = Paint()
+        paint.style = Paint.Style.FILL
+        paint.color = Color.parseColor(getColorStringByID(medication.color_id))
+        paint.isAntiAlias = true
+        iconCanvas.drawRoundRect(RectF(0f, 0f, iconCanvas.width.toFloat(), iconCanvas.height.toFloat()), (drawable.intrinsicWidth * 0.2).toFloat(), (drawable.intrinsicWidth * 0.2).toFloat(), paint)
+        drawable.setBounds((drawable.intrinsicWidth * 0.1).toInt(), (drawable.intrinsicWidth * 0.1).toInt(), iconCanvas.width - (drawable.intrinsicWidth * 0.1).toInt(), iconCanvas.height - (drawable.intrinsicWidth * 0.1).toInt())
+        drawable.draw(iconCanvas)
+
         // Set the notification content
         val mBuilder = NotificationCompat.Builder(context, context.getString(R.string.channel_id))
             .setSmallIcon(R.drawable.ic_pill_v5)
             .setContentTitle(title)
-            .setContentText("Notes:...")
-            .setStyle(NotificationCompat.BigTextStyle()
-                .bigText("Notes: " + medication.notes))
             .setPriority(NotificationCompat.PRIORITY_MAX)
             .setContentIntent(pendingIntent)
+            .setLargeIcon(iconBitmap)
+            .setAutoCancel(true)
+
+        if(medication.notes.isNotEmpty()) {
+            mBuilder.setContentText("Notes:...")
+                .setStyle(NotificationCompat.BigTextStyle()
+                    .bigText("Notes: " + medication.notes))
+        }
 
         val notificationManager = NotificationManagerCompat.from(context)
         // notificationId is a unique int for each notification that you must define
