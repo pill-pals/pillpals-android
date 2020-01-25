25c58e1aa943c416f01038f28110b00ac5f1218d
unknown <nicoiuorio@gmail.com>
2020-01-24 20:57:27 -0500

    version 2

--- app/src/main/java/com/pillpals/pillbuddies/ui/EditMedicationIcon.kt
+++ app/src/main/java/com/pillpals/pillbuddies/ui/EditMedicationIcon.kt
@@ -19,10 +19,15 @@ import android.widget.ImageView
 import android.widget.Toast
 import io.realm.Realm
 import androidx.cardview.widget.CardView
+import com.google.android.flexbox.FlexboxLayout
 import com.google.android.material.card.MaterialCardView
+import com.pillpals.pillbuddies.data.model.Photos
 import com.pillpals.pillbuddies.helpers.DatabaseHelper.Companion.getColorStringByID
 import com.pillpals.pillbuddies.helpers.DatabaseHelper.Companion.getIconByID
+import io.realm.RealmObject
+import io.realm.RealmResults
 import kotlinx.android.synthetic.main.activity_edit_medication_icon.*
+import java.util.*
 
 
 class EditMedicationIcon : AppCompatActivity() {
@@ -35,6 +40,7 @@ class EditMedicationIcon : AppCompatActivity() {
     public lateinit var shadesColorList : LinearLayout
     public lateinit var firstIconList: LinearLayout
     public lateinit var photoButton: Button
+    public lateinit var photoList: FlexboxLayout
     var colorString = "#FFFFFF"
     var imageDrawable = "ic_pill_v5"
 
@@ -42,6 +48,8 @@ class EditMedicationIcon : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         Realm.init(this)
         setContentView(R.layout.activity_edit_medication_icon)
+        val list: MutableList<String> = ArrayList()
+
 
         if(intent.hasExtra("color-string")) {
             colorString = intent.getStringExtra("color-string")
@@ -60,8 +68,11 @@ class EditMedicationIcon : AppCompatActivity() {
         shadesColorList = findViewById(R.id.shadesColorList)
 
         firstIconList = findViewById(R.id.firstIconList)
+        photoList = findViewById(R.id.photoList)
         photoButton = findViewById(R.id.photoButton)
 
+        photoList.removeAllViews()
+        populateGallery(readAllData(Photos::class.java) as RealmResults<out Photos>)
         addBorderToCards()
         for (i in 0 until lightColorList.getChildCount()) {
             val borderCard = lightColorList.getChildAt(i) as CardView
@@ -113,8 +124,11 @@ class EditMedicationIcon : AppCompatActivity() {
 
         photoButton.setOnClickListener {
             if(this.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY)){
-                val addIntent = Intent(this, DrugGallery::class.java)
-                startActivityForResult(addIntent, 1)
+                //val addIntent = Intent(this, DrugGallery::class.java)
+                //startActivityForResult(addIntent, 1)
+
+                dispatchTakePictureIntent()
+
             }else{
                 Toast.makeText(applicationContext, "Your device does not have a camera", Toast.LENGTH_SHORT).show()
             }
@@ -191,9 +205,26 @@ class EditMedicationIcon : AppCompatActivity() {
                 borderCard.setCardBackgroundColor(Color.parseColor("#00FFFFFF"))
             }
         }
+
+        for (i in 0 until photoList.getChildCount()){
+            val container = photoList.getChildAt(i) as CardView
+            container.setCardBackgroundColor(Color.parseColor("#00FFFFFF"))
+            container.setCardElevation(0f)
+
+            val borderCard = container.getChildAt(0) as CardView
+            val card = borderCard.getChildAt(0) as CardView
+            val image = card.getChildAt(0) as ImageView
+            val cardImageDrawable = image.tag
+            if (cardImageDrawable == imageDrawable) {
+                borderCard.setCardBackgroundColor(Color.parseColor("#FFFFFF"))
+            }
+            else {
+                borderCard.setCardBackgroundColor(Color.parseColor("#00FFFFFF"))
+            }
+        }
     }
 
-    /*private fun dispatchTakePictureIntent() {
+    private fun dispatchTakePictureIntent() {
         Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
             takePictureIntent.resolveActivity(packageManager)?.also {
                 startActivityForResult(takePictureIntent, 1)
@@ -205,7 +236,33 @@ class EditMedicationIcon : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == 1 && resultCode == RESULT_OK) {
             val imageBitmap = data!!.extras!!.get("data") as Bitmap
-            //imageView.setImageBitmap(imageBitmap)
+            Realm.getDefaultInstance().executeTransaction{
+                var newPhoto = it.createObject(Photos::class.java, UUID.randomUUID().toString())
+                newPhoto.icon = DatabaseHelper.convertBitmapToByteArray(imageBitmap)
+            }
+
+            photoList.removeAllViews()
+            populateGallery(readAllData(Photos::class.java) as RealmResults<out Photos>)
+            addBorderToCards()
         }
-    }*/
+    }
+
+    private fun populateGallery(photos: RealmResults<out Photos>){
+        for(photo in photos){
+            val newBmp = DatabaseHelper.convertByteArrayToBitmap(photo.icon)
+            val galleryIcon = GalleryIconCard(this)
+
+            galleryIcon.image.setImageBitmap(newBmp)
+            galleryIcon.image.tag = photo.uid
+            galleryIcon.setOnClickListener {
+                imageDrawable = galleryIcon.image.tag as String
+                addBorderToCards()
+            }
+            photoList.addView(galleryIcon)
+        }
+    }
+
+    private fun readAllData(realmClass: Class<out RealmObject>): RealmResults<out RealmObject> {
+        return Realm.getDefaultInstance().where(realmClass).findAll()
+    }
 }
