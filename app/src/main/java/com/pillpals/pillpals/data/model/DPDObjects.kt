e045c2339f8f05e88caa3e3314187fc19dae4197
William Harvey <williamnharvey@gmail.com>
2020-02-01 23:12:31 -0500

    Drug product database objects linked to medications (#110)

---
+++ app/src/main/java/com/pillpals/pillpals/data/model/DPDObjects.kt
@@ -0,0 +1,17 @@
+package com.pillpals.pillpals.data.model
+
+import io.realm.RealmObject
+import java.util.Date
+import io.realm.RealmList
+import io.realm.annotations.PrimaryKey
+import io.realm.annotations.LinkingObjects
+import io.realm.RealmResults
+
+open class DPDObjects(
+    @PrimaryKey var dpd_id: Int = 0,
+    var name: String = "",
+    var administrationRoutes: RealmList<String> = RealmList(),
+    var activeIngredients: RealmList<String> = RealmList(),
+    var dosageString: String = "",
+    var medications: RealmList<Medications> = RealmList()
+) : RealmObject(){}
