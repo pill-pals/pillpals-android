7a67c0a76861ce4f34af53a2f5846f09c7e85076
William Harvey <williamnharvey@gmail.com>
2020-03-14 13:17:08 -0400

    Data reader bug fix (#208)
    * Fix bug when no deleted date for schedule
    
    * More bug

--- data-reader.js
+++ data-reader.js
@@ -1,4 +1,6 @@
-const dataString = ``;
+const dataString = `
+
+`;
 const dataName = "output"
 
 const sh = require('run-sh');
@@ -17,25 +19,39 @@ const data = JSON.parse(dataString);
 const pin = [{pin: data.pin}];
 const logs = data.logs;
 const moodLogs = data.mood_logs;
-const schedules = data.schedules;
-const medications = data.medications;
-const quizzes = data.quizzes;
-
-const questionReducer = (accumulator, currentValue) => {
-	if(!currentValue.hasOwnProperty("medication_uid")) {
+const schedulesReducer = (accumulator, currentValue) => {
+	if(!currentValue.hasOwnProperty("deletedDate")) {
 		currentValue = {
-			correctAnswer: currentValue.correctAnswer,
-    		medication_uid: "",
-    		quiz_uid: currentValue.quiz_uid,
-    		template_id: currentValue.template_id,
-    		uid: currentValue.uid
+			deleted: currentValue.deleted,
+		    deletedDate: '',
+		    log_uids: currentValue.log_uids,
+		    medication_uid: currentValue.medication_uid,
+		    repetitionCount: currentValue.repetitionCount,
+		    repetitionUnit: currentValue.repetitionUnit,
+		    startDate: currentValue.startDate,
+		    uid: currentValue.uid
 		}
 	}
 	accumulator.push(currentValue);
 	return accumulator
 }
-const questions = data.questions.reduce(questionReducer, []);
+const schedules = data.schedules.reduce(schedulesReducer, []);
+const medications = data.medications;
+const quizzes = data.quizzes;
 
+const questionReducer = (accumulator, currentValue) => {
+	currentValue = {
+		correctAnswer: currentValue.correctAnswer === undefined ? "" : currentValue.correctAnswer,
+		medication_uid: currentValue.medication_uid || "",
+		quiz_uid: currentValue.quiz_uid || "",
+		template_id: currentValue.template_id,
+		uid: currentValue.uid || "",
+		userAnswer: currentValue.userAnswer === undefined ? "" : currentValue.userAnswer
+	}
+	accumulator.push(currentValue);
+	return accumulator
+}
+const questions = data.questions.reduce(questionReducer, []);
 const visit_logs = data.visit_logs;
 
 const output = convertToCSV(pin) +
